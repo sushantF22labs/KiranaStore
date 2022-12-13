c3987d70f7aa610eb74e741b09ce218f144dbc82
sushantF22labs <sushant.pagare@f22labs.com>
2022-12-13 13:17:03 +0530

    completed

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-const express= require("express");
-const {connection} = require("./config/db");
+const express = require("express");
+const { connection } = require("./config/db");
 const { productController } = require("./routes/products.route");
-require('dotenv').config()
-const app= express()
-var cors = require('cors')
-const PORT= process.env.PORT || 8080;
+require("dotenv").config();
+const app = express();
+var cors = require("cors");
+const PORT = process.env.PORT || 8080;
 app.use(express.json());
-app.use(cors())     
-app.get("/", (req,res)=>{
-    res.send("welcome to homepage")
-    console.log("Welcome to Homepage");
-})
-app.use("/products", productController)
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("welcome to homepage");
+  console.log("Welcome to Homepage");
+});
+app.use("/products", productController);
 
 app.listen(PORT, async () => {
-    try{
-        await connection;
-        console.log("Connected to db")
-    }
-    catch(err){
-        console.log("something went wrong");
-        console.log(err)
-    }
-    console.log(`listening on PORT ${PORT}`)
-})
+  try {
+    await connection;
+    console.log("Connected to db");
+  } catch (err) {
+    console.log("something went wrong");
+    console.log(err);
+  }
+  console.log(`listening on PORT ${PORT}`);
+});
--- models/product.model.js
+++ models/product.model.js
@@ -1,18 +1,18 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-    Title : {type : String, require : true},
-    Price : {type : Number, require : true},
-    Category:{
-       quality: {type : String, default : null}
-    },
-    Description:{type : String, require : true},
-    Img:{type:String,require: true},
-    userId : {type : String, require : true}
+  Title: { type: String, require: true },
+  Price: { type: Number, require: true },
+  Category: {
+    quality: { type: String, default: null },
+  },
+  Description: { type: String, require: true },
+  Img: { type: String, require: true },
+  userId: { type: String, require: true },
 });
 
-const ProductModel = mongoose.model("product", productSchema)
+const ProductModel = mongoose.model("product", productSchema);
 
 module.exports = {
-    ProductModel
-}
+  ProductModel,
+};
