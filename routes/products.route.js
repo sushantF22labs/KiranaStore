7b589393b6930010c48f8213ebff9c639f202c76
sushant-F22labs <sushant.pagare@f22labs.com>
2022-12-14 16:59:53 +0530

    updated

--- routes/products.route.js
+++ routes/products.route.js
@@ -4,14 +4,30 @@ require("dotenv").config();
 const { ProductModel } = require("../models/product.model");
 
 const productController = Router();
-
+// Getting data
 productController.get("/", async (req, res) => {
   let {skip,limit}= req.query;
-  const result = await ProductModel.find().skip(skip).limit(limit);
-  const count= await ProductModel.find().count()
-    res.send({result,count});
-})
+  const data = await ProductModel.find().skip(skip).limit(limit);
 
+  const count= await ProductModel.find().count();
+
+    res.send({data,count});
+
+})
+// Search functionality
+productController.get("/:key", async (req, res) => {
+  console.log(req.params.key);
+  const data = await ProductModel.find(
+    {
+      "$or":[
+        {"title":{$regex: req.params.key}}
+      ]
+    }
+  )
+  res.send(data)
+});
+
+//getting data by _id
 productController.get("/:id", async (req, res) => {
   let {skip,limit}= req.query;
   const result = await ProductModel.findById(req.params.id).skip(skip).limit(limit);
@@ -19,6 +35,7 @@ productController.get("/:id", async (req, res) => {
   res.send({result,count});
 });
 
+//Adding data
 productController.post("/", async (req, res) => {
   const payload = req.body;
   const new_prod = new ProductModel(payload);
@@ -26,18 +43,21 @@ productController.post("/", async (req, res) => {
   res.send(new_prod);
 });
 
+//Update data 
 productController.patch("/:id",async(req,res)=>{
    const data=await ProductModel.findByIdAndUpdate(req.params.id,req.body)
   console.log("product updated");
   res.send(data)
 })
 
+// Update data
 productController.put("/:id",async(req,res)=>{
   const data=await ProductModel.findByIdAndUpdate(req.params.id,req.body)
   console.log("product updated");
   res.send(data)
 })
 
+//Delete by _id
 productController.delete("/:id", async (req, res) => {
   const deletedProd = await ProductModel.findOneAndDelete({_id: req.params.id})
   res.send(deletedProd);
